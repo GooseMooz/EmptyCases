be94d2ae56f3abea0f187ca419b5c6536a1c556e
GooseMooz <goosev.goose@gmail.com>
2023-11-06 00:41:58 -0800

    Initial commit

---
+++ src/main/java/smp/plugin/GooseMooz/EmptyCases.java
@@ -0,0 +1,17 @@
+package smp.plugin.GooseMooz;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EmptyCases extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
