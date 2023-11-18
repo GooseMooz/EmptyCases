54f56d9fb952d05a997a5d282671e4f7d221a9b0
GooseMooz <goosev.goose@gmail.com>
2023-11-17 22:33:21 -0800

    Not sure about using a book.

--- src/main/java/smp/plugin/GooseMooz/GUIListener.java
+++ src/main/java/smp/plugin/GooseMooz/GUIListener.java
@@ -1,16 +1,15 @@
 package smp.plugin.GooseMooz;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.entity.Player;
-import org.bukkit.event.block.SignChangeEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.metadata.FixedMetadataValue;
 import smp.plugin.GooseMooz.Menu.CreateCaseMenu;
+import smp.plugin.GooseMooz.SupportFunctions.PlayerNameInput;
 
 public class GUIListener implements Listener {
     public static Inventory menu = CreateCaseMenu.initialMenu();
@@ -41,6 +40,7 @@ public class GUIListener implements Listener {
                 player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, (float) 0.25, 0);
             } else if (slot == 1) {
                 // Open Title Editing Sign
+                PlayerNameInput.createNameInput(player);
             }
         }
     }
--- src/main/java/smp/plugin/GooseMooz/SupportFunctions/PlayerNameInput.java
+++ src/main/java/smp/plugin/GooseMooz/SupportFunctions/PlayerNameInput.java
@@ -1,9 +1,33 @@
 package smp.plugin.GooseMooz.SupportFunctions;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.metadata.FixedMetadataValue;
+import smp.plugin.GooseMooz.EmptyCases;
 
 public class PlayerNameInput {
     public static void createNameInput (Player player) {
-        //TODO: Use NMS HERE
+        //TODO: Use BOOK HERE
+        // Create a written book
+        ItemStack book = new ItemStack(Material.WRITABLE_BOOK);
+
+        // Get the BookMeta
+        BookMeta meta = (BookMeta) book.getItemMeta();
+
+        // Add some pages
+        meta.addPage("Page 1 text", "Page 2 text", "Page 3 text");
+
+        // Apply the BookMeta to the book
+        book.setItemMeta(meta);
+
+        // Give the book to the player
+        player.getInventory().addItem(book);
+
+        player.openBook(book);
+        System.out.println(book.getItemMeta());
     }
 }
