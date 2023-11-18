2f05e6fca4640f2574f20ab304adce9d46b03d3b
GooseMooz <goosev.goose@gmail.com>
2023-11-18 01:20:02 -0800

    Using NMS 100%

--- src/main/java/smp/plugin/GooseMooz/SupportFunctions/PlayerNameInput.java
+++ src/main/java/smp/plugin/GooseMooz/SupportFunctions/PlayerNameInput.java
@@ -1,33 +1,14 @@
 package smp.plugin.GooseMooz.SupportFunctions;
 
-import net.kyori.adventure.text.Component;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BookMeta;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.metadata.FixedMetadataValue;
-import smp.plugin.GooseMooz.EmptyCases;
+import org.bukkit.craftbukkit.v1_20_R2.entity.CraftPlayer;
+import net.minecraft.server.level.ServerPlayer;
 
 public class PlayerNameInput {
     public static void createNameInput (Player player) {
-        //TODO: Use BOOK HERE
-        // Create a written book
-        ItemStack book = new ItemStack(Material.WRITABLE_BOOK);
+        //TODO: Use NMS... HERE
+        CraftPlayer craftPlayer = (CraftPlayer) player;
+        ServerPlayer serverPlayer = craftPlayer.getHandle();
 
-        // Get the BookMeta
-        BookMeta meta = (BookMeta) book.getItemMeta();
-
-        // Add some pages
-        meta.addPage("Page 1 text", "Page 2 text", "Page 3 text");
-
-        // Apply the BookMeta to the book
-        book.setItemMeta(meta);
-
-        // Give the book to the player
-        player.getInventory().addItem(book);
-
-        player.openBook(book);
-        System.out.println(book.getItemMeta());
     }
 }
