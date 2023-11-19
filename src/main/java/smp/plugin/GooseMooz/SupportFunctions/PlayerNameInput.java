95c110bda2d3737e7eb02816d154f31ae005ad48
GooseMooz <goosev.goose@gmail.com>
2023-11-18 17:38:58 -0800

    Using sendBlockUpdate and sendBlockChange implementation

--- src/main/java/smp/plugin/GooseMooz/SupportFunctions/PlayerNameInput.java
+++ src/main/java/smp/plugin/GooseMooz/SupportFunctions/PlayerNameInput.java
@@ -1,16 +1,21 @@
 package smp.plugin.GooseMooz.SupportFunctions;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.network.protocol.game.ClientboundOpenSignEditorPacket;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.TileState;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
-import org.bukkit.craftbukkit.v1_20_R2.entity.CraftPlayer;
-import net.minecraft.server.level.ServerPlayer;
 
 public class PlayerNameInput {
     public static void createNameInput (Player player) {
         //TODO: USE void sendBlockUpdate();
-        player.sendBlockUpdate(new Location(player.getWorld(), 0, 0, 0), player.sendBlockChange(new Location(player.getWorld(), 0, 0, 0), ));
+        Material block = Material.OAK_SIGN;
+        Block bl = player.getWorld().getBlockAt(0, 0, 0);
+        BlockData data = Bukkit.createBlockData(block);
+        TileState tileState = (TileState) bl.getState();
+        player.sendBlockChange(new Location(player.getWorld(), 0, 0, 0), data);
+        player.sendBlockUpdate(new Location(player.getWorld(), 0, 0, 0), tileState);
     }
 }
